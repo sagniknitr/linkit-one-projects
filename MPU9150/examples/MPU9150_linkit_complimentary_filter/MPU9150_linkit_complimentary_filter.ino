f1e95cdfc812a81818dc791be4b0611d11bf8bd0
Sagnik Basu <113ec0199@nitrkl.ac.in>
2017-03-08 00:49:06 -0800

    Add files via upload

---
+++ MPU9150/examples/MPU9150_linkit_complimentary_filter/MPU9150_linkit_complimentary_filter.ino
@@ -0,0 +1,146 @@
+// I2C device class (I2Cdev) demonstration Arduino sketch for MPU9150
+// 1/4/2013 original by Jeff Rowberg <jeff@rowberg.net> at https://github.com/jrowberg/i2cdevlib
+//          modified by Aaron Weiss <aaron@sparkfun.com>
+//
+// Changelog:
+//     2011-10-07 - initial release
+//     2013-1-4 - added raw magnetometer output
+
+/* ============================================
+I2Cdev device library code is placed under the MIT license
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+===============================================
+*/
+
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#include "Wire.h"
+
+// I2Cdev and MPU9150 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "I2Cdev.h"
+#include "MPU9150.h"
+#include "helper_3dmath.h"
+
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for InvenSense evaluation board)
+// AD0 high = 0x69
+MPU9150 accelGyroMag;
+
+
+//#define M_PI 3.14159265359      
+ 
+#define dt 0.01             // 10 ms sample rate! 
+#define ACCELEROMETER_SENSITIVITY 8192.0
+#define GYROSCOPE_SENSITIVITY 65.536
+
+int16_t a[3];
+int16_t g[3];
+int16_t m[3];
+
+#define LED_PIN 13
+bool blinkState = false;
+
+void setup() {
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    Wire.begin();
+
+    // initialize serial communication
+    // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
+    // it's really up to you depending on your project)
+    Serial.begin(115200);
+
+    // initialize device
+    Serial.println("Initializing I2C devices...");
+    accelGyroMag.initialize();
+
+    // verify connection
+    Serial.println("Testing device connections...");
+    Serial.println(accelGyroMag.testConnection() ? "MPU9150 connection successful" : "MPU9150 connection failed");
+
+    // configure Arduino LED for
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+    // read raw accel/gyro/mag measurements from device
+    accelGyroMag.getMotion9(&a[0], &a[1], &a[2], &g[0], &g[1], &g[2], &m[0], &m[1], &m[2]);
+
+    // these methods (and a few others) are also available
+   // accelGyroMag.getAcceleration(&ax, &ay, &az);
+   // accelGyroMag.getRotation(&gx, &gy, &gz);
+
+    // display tab-separated accel/gyro/mag x/y/z values
+// Serial.print("a/g/m:\t");
+ /*Serial.print(a[0]); Serial.print("\t");
+  Serial.print(a[1]); Serial.print("\t");
+ Serial.print(a[2]); Serial.print("\t");  
+ Serial.print(g[0]); Serial.print("\t");
+  Serial.print(g[1]); Serial.print("\t");
+  Serial.print(g[2]); Serial.print("\t");
+  Serial.print(int(m[0])*int(m[0])); Serial.print("\t");
+   Serial.print(int(m[1])*int(m[1])); Serial.print("\t");
+   Serial.println(int(m[2])*int(m[2])); */
+
+   ComplementaryFilter(a,g);
+
+    //const float N = 256;
+   // float mag = mx*mx/N + my*my/N + mz*mz/N;
+
+  //  Serial.print(mag); Serial.print("\t");
+  //  for (int i=0; i<mag; i++)
+       // Serial.print("*");
+   // Serial.print("\n");
+
+    // blink LED to indicate activity
+    blinkState = !blinkState;
+    //digitalWrite(LED_PIN, blinkState);
+    delay(50);
+}
+
+   
+ 
+void ComplementaryFilter(int16_t accData[3], int16_t gyrData[3])
+{
+    float pitchAcc, rollAcc;               
+    float pitch, roll;
+    // Integrate the gyroscope data -> int(angularSpeed) = angle
+    pitch += ((float)gyrData[0] / GYROSCOPE_SENSITIVITY) * dt; // Angle around the X-axis
+    roll -= ((float)gyrData[1] / GYROSCOPE_SENSITIVITY) * dt;    // Angle around the Y-axis
+ 
+    // Compensate for drift with accelerometer data if !bullshit
+    // Sensitivity = -2 to 2 G at 16Bit -> 2G = 32768 && 0.5G = 8192
+    int forceMagnitudeApprox = abs(accData[0]) + abs(accData[1]) + abs(accData[2]);
+    if (forceMagnitudeApprox > 8192 && forceMagnitudeApprox < 32768)
+    {
+  // Turning around the X axis results in a vector on the Y-axis
+        pitchAcc = atan2f((float)accData[1], (float)accData[2]) * 180 / M_PI;
+        pitch = pitch * 0.98 + pitchAcc * 0.02;
+ 
+  // Turning around the Y axis results in a vector on the X-axis
+        rollAcc = atan2f((float)accData[0], (float)accData[2]) * 180 / M_PI;
+        roll = roll * 0.98 + rollAcc * 0.02;
+    }
+
+    Serial.print(roll);
+      Serial.print(" ");
+    Serial.println(pitch);
+} 
