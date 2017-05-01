5d3e385379936c1b5a0cbc1e677873b68db63b98
Sagnik Basu <113ec0199@nitrkl.ac.in>
2017-05-01 04:28:05 -0700

    Add files via upload

---
+++ LinkIt_MCS_20150919/LinkIt_MCS_20150919.ino
@@ -0,0 +1,101 @@
+#include <LGPRS.h>
+#include <LGPRSClient.h>
+#include <LGPRSServer.h>
+
+#include <LGPS.h>
+#include <HttpClient.h>
+#include <LTask.h>
+#include <LWiFi.h>
+#include <LWiFiClient.h>
+#include <LDateTime.h>
+  #define WIFI_AP "chidori"
+  #define WIFI_PASSWORD "shaggy95"
+#define WIFI_AUTH LWIFI_WPA  // choose from LWIFI_OPEN, LWIFI_WPA, or LWIFI_WEP.
+#define per 50
+#define per1 3
+#define DEVICEID "DEHGaKt6"
+#define DEVICEKEY "D6HN8LXwej3VOsbd"
+#define SITE_URL "api.mediatek.com"
+
+gpsSentenceInfoStruct info;
+char buff[256];
+double latitude;
+double longitude;
+
+char buffer_latitude[8];
+char buffer_longitude[8];
+LGPRSClient c;
+//LWiFiClient c;
+unsigned int rtc;
+unsigned int lrtc;
+unsigned int rtc1;
+unsigned int lrtc1;
+char port[4]={0};
+char connection_info[21]={0};
+char ip[15]={0};             
+int portnum;
+int val = 0;
+String tcpdata = String(DEVICEID) + "," + String(DEVICEKEY) + ",0";
+String tcpcmd_led_on = "LED_CONTROL,1";
+String tcpcmd_led_off = "LED_CONTROL,0";
+String upload_led;
+
+LGPRSClient c2;
+//LWiFiClient c2;
+HttpClient http(c2);
+
+void setup()
+{
+    Serial.begin(115200);
+  pinMode(13, OUTPUT);
+  Serial.println("Task Begin");
+  LTask.begin();
+  Serial.println("LWifi Begin");
+  LWiFi.begin();
+
+  LGPS.powerOn();
+  //while(!Serial);
+  
+  AP_connect();
+  getconnectInfo();
+  connectTCP();
+  Serial.println("==============================");
+}
+
+void loop()
+{
+  String tcpcmd="";
+  while (c.available())
+   {
+      int v = c.read();
+      if (v != -1)
+      {
+        Serial.print((char)v);
+        tcpcmd += (char)v;
+        if (tcpcmd.substring(40).equals(tcpcmd_led_on)){
+          digitalWrite(13, HIGH);
+          Serial.print("Switch LED ON ");
+          tcpcmd="";
+        }else if(tcpcmd.substring(40).equals(tcpcmd_led_off)){  
+          digitalWrite(13, LOW);
+          Serial.print("Switch LED OFF");
+          tcpcmd="";
+        }
+      }
+   }
+
+  LDateTime.getRtc(&rtc);
+  if ((rtc - lrtc) >= per) {
+    heartBeat();
+    lrtc = rtc;
+  }
+  //Check for report datapoint status interval
+  LDateTime.getRtc(&rtc1);
+  if ((rtc1 - lrtc1) >= per1) {
+    
+    uploadstatus();
+    GPS_receive();
+    uploadGPS();
+    lrtc1 = rtc1;
+  }
+}
---
+++ wifi_test/wifi_test.ino
@@ -0,0 +1,107 @@
+ #include <LTask.h>
+  #include <LWiFi.h>
+  #include <LWiFiClient.h>
+
+  #define WIFI_AP "chidori"
+  #define WIFI_PASSWORD "shaggy95"
+  #define WIFI_AUTH LWIFI_WPA  // choose from LWIFI_OPEN, LWIFI_WPA, or LWIFI_WEP.
+
+  // Ubidots information
+
+  #define URL    "things.ubidots.com"
+  #define TOKEN  "Sxxxxxxxxx9m4wX"          // replace with your Ubidots token generated in your profile tab
+  #define VARID1 "55fda54a762542789fe3415b"                // create a variable in Ubidots and put its ID here (http://app.ubidots.com/ubi/datasources/)
+
+
+  void setup()
+  {
+    LTask.begin();
+    LWiFi.begin();
+    Serial.begin(19200);
+
+    // keep retrying until connected to AP
+    Serial.println("Connecting to AP");
+    while (0 == LWiFi.connect(WIFI_AP, LWiFiLoginInfo(WIFI_AUTH, WIFI_PASSWORD)))
+    {
+      delay(1000);
+    }
+  }
+
+  // I plan to use this function in the future to reconnect to WiFi if the connection drops:
+  boolean wifi_status(LWifiStatus ws){
+    switch(ws){
+      case LWIFI_STATUS_DISABLED:
+        return false;
+      break;
+      case LWIFI_STATUS_DISCONNECTED:
+        return false;
+      break;
+      case LWIFI_STATUS_CONNECTED:
+        return true;
+      break;
+    }
+    return false;
+  }
+
+
+  void loop()
+  {   
+    Serial.println("Connecting to Ubidots...");
+    LWiFiClient c;
+    while (!c.connect(URL, 80))
+    {
+      Serial.println("Retrying to connect...");
+      delay(100);
+    }
+
+    Serial.println("Connected!");
+
+    while(1){
+      LWifiStatus ws = LWiFi.status();
+      boolean status = wifi_status(ws);
+      if(!status){
+        Serial.println("Connecting to AP");
+        while (0 == LWiFi.connect(WIFI_AP, LWiFiLoginInfo(WIFI_AUTH, WIFI_PASSWORD)))
+        {
+          delay(500);
+        }
+      }
+      String payload = "{\"value\":"+ String(analogRead(A0)) + "}";
+      String le = String(payload.length());    
+
+      if(!c.connected()){
+        while (!c.connect(URL, 80)){
+          delay(100);
+        }
+        Serial.println("Client reconnected!");
+      }   
+
+      // Build HTTP POST request
+      c.print(F("POST /api/v1.6/variables/"));
+//      c.print(ID);
+      c.println(F("/values HTTP/1.1"));
+      c.println(F("User-Agent: LinKit One/1.0"));
+      c.print(F("X-Auth-Token: "));
+      c.println(TOKEN);
+      c.println(F("Connection: close"));
+      c.println(F("Content-Type: application/json"));
+      c.print(F("Content-Length: "));
+      c.println(le);
+      c.print(F("Host: "));
+      c.println(URL);
+      c.println(); 
+      c.println(payload);
+      c.println(); 
+
+      int v;
+      while(c.available()){  
+        v = c.read();
+        if(v < 0){
+          Serial.println("No response.");
+          break;
+        }
+        // Serial.print((char)v);  // Add this line to the code in test case
+      }
+      delay(1000);
+    }
+  }
